3691b25ffe5b5545f915963cd051f79c9cbcb208
Suhail Ansari <SuhailAnsari0201@outlook.com>
2020-08-24 15:50:08 +0530

    initial commit

---
+++ scripts/auth.js
@@ -0,0 +1,86 @@
+// Listen for auth status change
+auth.onAuthStateChanged((user) => {
+  if (user) {
+    db.collection("guides").onSnapshot(
+      (snapshot) => {
+        setupGides(snapshot.docs);
+        setupUI(user);
+      },
+      (error) => console.log(error.message)
+    );
+  } else {
+    console.log("User Logged out");
+    setupGides([]);
+    setupUI();
+  }
+});
+
+// Create new Guide
+const createForm = document.querySelector("#create-form");
+createForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  db.collection("guides")
+    .add({
+      title: createForm["title"].value,
+      content: createForm["content"].value,
+    })
+    .then(() => {
+      const modal = document.querySelector("#modal-create");
+      M.Modal.getInstance(modal).close();
+      createForm.reset();
+    })
+    .catch((error) => alert(error.message));
+});
+
+// Signup
+const signupForm = document.querySelector("#signup-form");
+signupForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const email = signupForm["signup-email"].value;
+  const password = signupForm["signup-password"].value;
+  // signup the user
+  auth
+    .createUserWithEmailAndPassword(email, password)
+    .then((user) => {
+      // user.user.updateProfile({
+      //   displayName: signupForm["signup-username"].value,
+      // });
+      return db.collection("users").doc(user.user.uid).set({
+        username: signupForm["signup-username"].value,
+        bio: signupForm["signup-bio"].value,
+      });
+    })
+    .then(() => {
+      const modal = document.querySelector("#modal-signup");
+      M.Modal.getInstance(modal).close();
+      signupForm.reset();
+    })
+    .catch((error) => alert(error.message));
+});
+
+// Login
+const loginForm = document.querySelector("#login-form");
+loginForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const email = loginForm["login-email"].value;
+  const password = loginForm["login-password"].value;
+  // login the user
+  auth
+    .signInWithEmailAndPassword(email, password)
+    .then((user) => {
+      const modal = document.querySelector("#modal-login");
+      M.Modal.getInstance(modal).close();
+      loginForm.reset();
+    })
+    .catch((error) => alert(error.message));
+});
+
+// Logout
+const logout = document.querySelector("#logout");
+logout.addEventListener("click", (e) => {
+  e.preventDefault();
+  auth.signOut().then();
+});
+
+// video 17 on way
